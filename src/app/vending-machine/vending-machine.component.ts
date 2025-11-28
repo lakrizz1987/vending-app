1a8d40660562a8af6d945d578fa4c303d8c319bc
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2025-11-28 17:08:57 +0200

    Adding vending displays and logic for valid coins, insert and cancel coins

--- src/app/vending-machine/vending-machine.component.ts
+++ src/app/vending-machine/vending-machine.component.ts
@@ -1,23 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../assets/product.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vending-machine',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vending-machine.component.html',
   styleUrl: './vending-machine.component.scss',
   standalone: true
 })
 
 export class VendingMachine implements OnInit {
-  constructor(private productService: ProductService) {
+  acceptedCoins: CoinsMap = {
+    BGN: [0.05, 0.1, 0.2, 0.5, 1, 2]
+  };
 
-  }
+  amount: number = 0;
+  currency: string = 'BGN';
+  balance: number = 0;
+  displayMessage: string = VendingMessages.INSERT_COIN;
 
-  ngOnInit(){
-    setTimeout(()=>{
+  constructor(
+    private productService: ProductService
+  ) { }
+
+  ngOnInit() {
+    setTimeout(() => {
 
       this.productService.getProducts().subscribe(p => console.log(p))
-    },2000)
+    }, 2000)
+  }
+
+  insertMoney() {
+    if (!this.amount) {
+      return;
+    }
+
+    if (this.isValidCoin(this.amount, this.currency)) {
+      this.balance = Number((this.balance + this.amount).toFixed(2));
+      this.amount = 0;
+    } else {
+      this.changeDisplayMessage(VendingMessages.INVALID_COIN);
+      this.amount = 0;
+    }
+  }
+
+  isValidCoin(amount: number, currency: string): boolean {
+    return this.acceptedCoins[currency]?.includes(amount) ?? false;
+  }
+
+  onCancel() {
+    this.amount = 0;
+    this.balance = 0;
+    this.changeDisplayMessage(VendingMessages.ORDER_CANCELED);
+  }
+
+  changeDisplayMessage(message: VendingMessages) {
+    this.displayMessage = message;
+    setTimeout(() => {
+      this.displayMessage = VendingMessages.INSERT_COIN;
+    }, 5000);
   }
 }
+
+
+type CoinsMap = {
+  [key: string]: number[];
+};
+
+export enum VendingMessages {
+  INSERT_COIN = 'Please insert coin !',
+  INVALID_COIN = 'Invalid coin !!!',
+  TAKE_PURCHASE = 'Please take your purchase !',
+  ORDER_CANCELED = 'Order canceled, please take your coins !'
+}
+

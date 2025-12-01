5ee66c7ca2bb6e0cea6b23c2aad8c4b358214295
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2025-12-01 20:57:11 +0200

    Cretaing adding form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Paths } from './enums/Paths.enum';
 import { VendingMachine } from './vending-machine/vending-machine.component';
 import { SettingsComponent } from './settings-component/settings.component';
+import { AddProduct } from './add-product/add-product.component';
 
 export const routes: Routes = [
     { path: '', component: VendingMachine },
     { path: Paths.Home, component: VendingMachine },
     { path: Paths.Settings, component: SettingsComponent },
+    { path: Paths.Create, component: AddProduct },
 ];
--- src/app/enums/Paths.enum.ts
+++ src/app/enums/Paths.enum.ts
@@ -1,4 +1,5 @@
 export enum Paths {
   Home = '',
   Settings = 'settings',
+  Create = 'create'
 }
